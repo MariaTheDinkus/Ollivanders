c31de243396aee318995ef0e067f7cb3c3d383a2
Maria <mariathedinkus@gmail.com>
2023-05-19 14:07:28 -0500

    feat: change cores around and their rarities

--- src/main/java/to/tinypota/ollivanders/registry/common/OllivandersCores.java
+++ src/main/java/to/tinypota/ollivanders/registry/common/OllivandersCores.java
@@ -9,15 +9,15 @@ import to.tinypota.ollivanders.common.util.WeightedRandomBag;
 public class OllivandersCores {
 	public static final WeightedRandomBag<Core> CORES = new WeightedRandomBag<>();
 	public static final Core EMPTY = OllivandersCores.register("empty", new Core(Items.AIR, 0));
-	public static final Core BONE = OllivandersCores.register("bone", new Core(Items.BONE, 80));
-	public static final Core SPIDER_EYE = OllivandersCores.register("spider_eye", new Core(Items.SPIDER_EYE, 80));
-	public static final Core ENDER_PEARL = OllivandersCores.register("ender_pearl", new Core(Items.ENDER_PEARL, 60));
-	public static final Core BLAZE_POWDER = OllivandersCores.register("blaze_powder", new Core(Items.BLAZE_POWDER, 60));
-	public static final Core EYE_OF_ENDER = OllivandersCores.register("eye_of_ender", new Core(Items.ENDER_EYE, 40));
-	public static final Core DRAGON_HEARTSTRING = OllivandersCores.register("dragon_heartstring", new Core(OllivandersItems.DRAGON_HEARTSTRING, 20));
+	public static final Core ENDER_PEARL = OllivandersCores.register("ender_pearl", new Core(Items.ENDER_PEARL, 35));
+	public static final Core BLAZE_POWDER = OllivandersCores.register("blaze_powder", new Core(Items.BLAZE_POWDER, 35));
+	public static final Core EYE_OF_ENDER = OllivandersCores.register("eye_of_ender", new Core(Items.ENDER_EYE, 35));
+	public static final Core ECHO_SHARD = OllivandersCores.register("echo_shard", new Core(Items.ECHO_SHARD, 25));
+	public static final Core HEART_OF_THE_SEA = OllivandersCores.register("heart_of_the_sea", new Core(Items.HEART_OF_THE_SEA, 25));
+	public static final Core DRAGON_HEARTSTRING = OllivandersCores.register("dragon_heartstring", new Core(OllivandersItems.DRAGON_HEARTSTRING, 40));
+	public static final Core UNICORN_TAIL_HAIR = OllivandersCores.register("unicorn_tail_hair", new Core(OllivandersItems.UNICORN_TAIL_HAIR, 40));
 	public static final Core PHOENIX_FEATHER = OllivandersCores.register("phoenix_feather", new Core(OllivandersItems.PHOENIX_FEATHER, 20));
-	public static final Core THESTRAL_TAIL_HAIR = OllivandersCores.register("thestral_tail_hair", new Core(OllivandersItems.THESTRAL_TAIL_HAIR, 20));
-	public static final Core UNICORN_TAIL_HAIR = OllivandersCores.register("unicorn_tail_hair", new Core(OllivandersItems.UNICORN_TAIL_HAIR, 20));
+	public static final Core THESTRAL_TAIL_HAIR = OllivandersCores.register("thestral_tail_hair", new Core(OllivandersItems.THESTRAL_TAIL_HAIR, 4));
 	
 	public static void init() {
 	
--- src/main/java/to/tinypota/ollivanders/registry/common/OllivandersSpells.java
+++ src/main/java/to/tinypota/ollivanders/registry/common/OllivandersSpells.java
@@ -4,16 +4,13 @@ import net.minecraft.entity.effect.StatusEffectInstance;
 import net.minecraft.entity.effect.StatusEffects;
 import net.minecraft.registry.Registry;
 import to.tinypota.ollivanders.Ollivanders;
-import to.tinypota.ollivanders.common.spell.ClearEffectSpell;
-import to.tinypota.ollivanders.common.spell.EffectSpell;
-import to.tinypota.ollivanders.common.spell.Spell;
-import to.tinypota.ollivanders.common.spell.SpellType;
+import to.tinypota.ollivanders.common.spell.*;
 import to.tinypota.ollivanders.common.util.SpellHelper;
 
 public class OllivandersSpells {
 	public static final Spell LUMOS = register("lumos", new EffectSpell("lumos", new StatusEffectInstance(StatusEffects.NIGHT_VISION, 20 * 30), new Spell.Settings().type(SpellType.SELF)));
 	public static final Spell NOX = register("nox", new ClearEffectSpell("nox", StatusEffects.NIGHT_VISION, new Spell.Settings().type(SpellType.SELF)));
-	//public static final Spell RAY = register("ray", new TestRaycastSpell("ray", new Spell.Settings().type(SpellType.RAYCAST)));
+	public static final Spell RAY = register("ray", new TestRaycastSpell("ray", new Spell.Settings().type(SpellType.RAYCAST)));
 	
 	public static void init() {
 	
