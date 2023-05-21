02fa4e0075b4142006a6bf6c38c5f384c1694912
Maria <mariathedinkus@gmail.com>
2023-05-20 19:02:07 -0500

    feat: make text box selectable and usable

--- src/main/java/to/tinypota/ollivanders/client/screen/FlooFireNameScreen.java
+++ src/main/java/to/tinypota/ollivanders/client/screen/FlooFireNameScreen.java
@@ -13,7 +13,7 @@ public class FlooFireNameScreen extends Screen {
 	private BlockPos pos;
 
     public FlooFireNameScreen() {
-        super(Text.literal("My GUI"));
+        super(Text.literal("Floo Fire Name Chooser"));
     }
 	
 	public FlooFireNameScreen(BlockPos pos) {
@@ -29,7 +29,8 @@ public class FlooFireNameScreen extends Screen {
         int textFieldY = (this.height - textFieldHeight) / 2;
 
         this.textField = new TextFieldWidget(this.textRenderer, textFieldX, textFieldY, textFieldWidth, textFieldHeight, Text.literal(""));
-        this.addDrawable(this.textField);
+        this.textField.setMaxLength(25);
+		this.addDrawable(this.textField);
 
         int buttonWidth = 100;
         int buttonHeight = 20;
@@ -39,6 +40,8 @@ public class FlooFireNameScreen extends Screen {
         this.submitButton = ButtonWidget.builder(Text.literal("Submit"), button -> {
 			
 		}).position(buttonX, buttonY).size(buttonWidth, buttonHeight).build();
+		
+		setInitialFocus(this.textField);
     }
 	
 	@Override
@@ -48,4 +51,33 @@ public class FlooFireNameScreen extends Screen {
         this.submitButton.render(context, mouseX, mouseY, delta);
         super.render(context, mouseX, mouseY, delta);
     }
+	
+	@Override
+	public boolean keyPressed(int keyCode, int scanCode, int modifiers) {
+		if (super.keyPressed(keyCode, scanCode, modifiers)) {
+			return true;
+		} else if (keyCode == 256) { // Escape key
+			this.close();
+			return true;
+		}
+		
+		// Forward the key pressed event to the text field
+		return this.textField.keyPressed(keyCode, scanCode, modifiers);
+	}
+	
+	@Override
+	public boolean charTyped(char chr, int modifiers) {
+		// Forward the char typed event to the text field
+		return this.textField.charTyped(chr, modifiers);
+	}
+	
+	@Override
+	public boolean mouseClicked(double mouseX, double mouseY, int button) {
+		if (super.mouseClicked(mouseX, mouseY, button)) {
+			return true;
+		}
+		
+		// Forward the mouse clicked event to the text field
+		return this.textField.mouseClicked(mouseX, mouseY, button);
+	}
 }
