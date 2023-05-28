5b32e6417a764665e895fe20ebd846035eee0b71
Maria <mariathedinkus@gmail.com>
2023-05-27 20:02:48 -0500

    fix: forgot some files

--- src/main/java/to/tinypota/ollivanders/common/block/sapling/LaurelSaplingGenerator.java
+++ src/main/java/to/tinypota/ollivanders/common/block/sapling/LaurelSaplingGenerator.java
@@ -8,12 +8,13 @@ import net.minecraft.registry.RegistryKey;
 import net.minecraft.util.math.random.Random;
 import net.minecraft.world.gen.feature.ConfiguredFeature;
 import net.minecraft.world.gen.feature.TreeConfiguredFeatures;
+import to.tinypota.ollivanders.registry.common.OllivandersFeatures;
 
-public class BirchSaplingGenerator
+public class LaurelSaplingGenerator
 extends SaplingGenerator {
     @Override
     protected RegistryKey<ConfiguredFeature<?, ?>> getTreeFeature(Random random, boolean bees) {
-        return bees ? TreeConfiguredFeatures.BIRCH_BEES_005 : TreeConfiguredFeatures.BIRCH;
+        return OllivandersFeatures.LAUREL_TREE;
     }
 }
 
--- src/main/java/to/tinypota/ollivanders/datagen/OllivandersLootTableGenerator.java
+++ src/main/java/to/tinypota/ollivanders/datagen/OllivandersLootTableGenerator.java
@@ -1,47 +1,47 @@
 package to.tinypota.ollivanders.datagen;
 
 import net.fabricmc.fabric.api.datagen.v1.FabricDataOutput;
-import net.fabricmc.fabric.api.datagen.v1.provider.FabricLanguageProvider;
+import net.fabricmc.fabric.api.datagen.v1.provider.FabricBlockLootTableProvider;
+import net.fabricmc.fabric.api.datagen.v1.provider.FabricLootTableProvider;
 import net.fabricmc.fabric.api.datagen.v1.provider.FabricTagProvider;
 import net.minecraft.block.Block;
+import net.minecraft.block.Blocks;
 import net.minecraft.registry.RegistryKeys;
 import net.minecraft.registry.RegistryWrapper;
 import net.minecraft.registry.tag.BlockTags;
 import net.minecraft.registry.tag.TagKey;
 import net.minecraft.util.Identifier;
 import to.tinypota.ollivanders.registry.builder.WoodBlockRegistry;
-import to.tinypota.ollivanders.registry.common.OllivandersItemGroups;
-import to.tinypota.ollivanders.registry.common.OllivandersItems;
 
 import java.util.concurrent.CompletableFuture;
 
-public class OllivandersBlockTagGenerator extends FabricTagProvider.BlockTagProvider {
-	public OllivandersBlockTagGenerator(FabricDataOutput output, CompletableFuture<RegistryWrapper.WrapperLookup> registriesFuture) {
-		super(output, registriesFuture);
+public class OllivandersLootTableGenerator extends FabricBlockLootTableProvider {
+	public OllivandersLootTableGenerator(FabricDataOutput output) {
+		super(output);
 	}
 	
 	@Override
-	protected void configure(RegistryWrapper.WrapperLookup arg) {
+	public void generate() {
 		for (var storage : WoodBlockRegistry.WOOD_BLOCK_STORAGES) {
-			var logTag = TagKey.of(RegistryKeys.BLOCK, new Identifier("c", storage.getName() + "_logs"));
-			getOrCreateTagBuilder(logTag).add(storage.getLog(), storage.getWood(), storage.getStrippedLog(), storage.getStrippedWood());
-			getOrCreateTagBuilder(BlockTags.WOODEN_BUTTONS).add(storage.getButton());
-			getOrCreateTagBuilder(BlockTags.WOODEN_DOORS).add(storage.getDoor());
-			getOrCreateTagBuilder(BlockTags.WOODEN_FENCES).add(storage.getFence());
-			getOrCreateTagBuilder(BlockTags.FENCE_GATES).add(storage.getFenceGate());
-			getOrCreateTagBuilder(BlockTags.LEAVES).add(storage.getLeaves());
-			getOrCreateTagBuilder(BlockTags.LOGS_THAT_BURN).addTag(logTag);
-			getOrCreateTagBuilder(BlockTags.OVERWORLD_NATURAL_LOGS).add(storage.getLog());
-			getOrCreateTagBuilder(BlockTags.PLANKS).add(storage.getPlanks());
-			getOrCreateTagBuilder(BlockTags.WOODEN_PRESSURE_PLATES).add(storage.getPressurePlate());
-			getOrCreateTagBuilder(BlockTags.SAPLINGS).add(storage.getSapling());
-			getOrCreateTagBuilder(BlockTags.WOODEN_SLABS).add(storage.getSlab());
-			getOrCreateTagBuilder(BlockTags.WOODEN_STAIRS).add(storage.getStairs());
-			getOrCreateTagBuilder(BlockTags.WOODEN_TRAPDOORS).add(storage.getTrapdoor());
-			getOrCreateTagBuilder(BlockTags.STANDING_SIGNS).add(storage.getSign());
-			getOrCreateTagBuilder(BlockTags.WALL_SIGNS).add(storage.getWallSign());
-			getOrCreateTagBuilder(BlockTags.CEILING_HANGING_SIGNS).add(storage.getHangingSign());
-			getOrCreateTagBuilder(BlockTags.WALL_HANGING_SIGNS).add(storage.getWallHangingSign());
+			addDrop(storage.getLog());
+			addDrop(storage.getWood());
+			addDrop(storage.getStrippedLog());
+			addDrop(storage.getStrippedWood());
+			addDrop(storage.getPlanks());
+			addDrop(storage.getStairs());
+			addDrop(storage.getSlab(), block -> slabDrops(storage.getSlab()));
+			addDrop(storage.getFence());
+			addDrop(storage.getFenceGate());
+			addDrop(storage.getDoor());
+			addDrop(storage.getTrapdoor());
+			addDrop(storage.getPressurePlate());
+			addDrop(storage.getButton());
+			addDrop(storage.getSign());
+			addDrop(storage.getWallSign(), block -> drops(storage.getSign()));
+			addDrop(storage.getHangingSign());
+			addDrop(storage.getWallHangingSign(), block -> drops(storage.getHangingSign()));
+			addDrop(storage.getSapling());
+			addDrop(storage.getLeaves(), block -> leavesDrops(block, storage.getSapling(), SAPLING_DROP_CHANCE));
 		}
 	}
 }
